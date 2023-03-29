66a3edbb7498c068ce8fba9a1f261ba71f9f24d4
Daniel Barcelo <danieldeveloper11@gmail.com>
2023-03-28 21:47:09 -0500

    Arreglo en cors

--- src/app.js
+++ src/app.js
@@ -53,7 +53,8 @@ app.get("/", (req, res) => {
 
 // Routes
 app.use(cors({
-  origin: 'http://localhost:5173'
+  origin: 'http://localhost:5173',
+  credentials: true
 }))
 app.use("/api/request", requestRoutes);
 app.use("/api/auth", authRoutes);
