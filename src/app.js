8305d1c9a8272e99388e5f49afe224fe2e455c1a
Daniel Barcelo <danieldeveloper11@gmail.com>
2023-04-27 17:48:41 -0500

    Cambio de servidor

--- src/app.js
+++ src/app.js
@@ -53,7 +53,8 @@ app.get("/", (req, res) => {
 
 // Routes
 app.use(cors({
-  origin: 'https://permisosgrumetesarc.netlify.app',
+  // origin: 'https://permisosgrumetesarc.netlify.app',
+  origin: 'http://localhost:5173',
   credentials: true,
 }))
 app.use("/api/request", requestRoutes);
--- src/database.js
+++ src/database.js
@@ -2,7 +2,8 @@ import mongoose from "mongoose";
 
 mongoose
   .connect(
-    `mongodb://${process.env.MONGOUSER}:${process.env.MONGOPASSWORD}@${process.env.MONGOHOST}:${process.env.MONGOPORT}`,
+    // `mongodb://${process.env.MONGOUSER}:${process.env.MONGOPASSWORD}@${process.env.MONGOHOST}:${process.env.MONGOPORT}`,
+    'mongodb://127.0.0.1:27017',
     {
       useNewUrlParser: true,
       useUnifiedTopology: true,
