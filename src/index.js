17c62ad02a9063dd931e65226bbd8893963bb947
DanielDominguez <84419904+Daniel110702@users.noreply.github.com>
2023-04-28 12:55:04 +0000

    Cambio de Puerto

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import app from "./app";
 import "./database";
 import './config'
 
-app.set("port", process.env.PORT || 3000);
+app.set("port", process.env.PORT || 443);
 
 app.listen(app.get("port"), () => {
   console.log("server on port", app.get("port"));
