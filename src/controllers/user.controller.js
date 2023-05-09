4024c03d8581a69d01c6c970846ae425205f10fc
Fernando Zapata <70167160+fjzapata27@users.noreply.github.com>
2023-05-09 17:17:41 -0500

    Update user.controller.js
    Ajuste busqueda de usuarios administradores

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -8,7 +8,7 @@ export const createUser = (req, res) => {
 export const getReqAdmin = async (req, res) => {
     try {
         const role = await Role.findOne({ name: "admin" });
-        const requests = await User.find({ roles: { $in: [role._id] } }).select('_id', 'username');
+        const requests = await User.find({ roles: { $in: [ObjectId(role._id)] } });
         res.json(requests);
     } catch (error) {
         return res.status(500).json({ message: "Something goes wrong" });
